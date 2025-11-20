ae43d98110754d3f7576c117419dd7171ed63682
SushyDev <mail@sushy.dev>
2025-11-20 22:29:54 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+	};
+
+	outputs = { self, nixpkgs }:
+		let
+			supportedSystems = nixpkgs.lib.platforms.all;
+			devShells = 
+				nixpkgs.lib.genAttrs supportedSystems (system: 
+				let
+					pkgs = import nixpkgs { inherit system; };
+					inherit (pkgs) stdenv;
+				in 
+				{
+					default = pkgs.mkShell {
+						buildInputs = [
+							pkgs.elixir
+							pkgs.watchman
+							pkgs.inotify-tools
+						];
+
+						shellHook = ''
+							echo "Elixir version: $(elixir --version)"
+						'';
+					};
+				}
+			);
+		in
+		let
+			supportedSystems = nixpkgs.lib.platforms.all;
+			packages = {};
+		in
+		{
+			inherit devShells packages;
+		};
+}
